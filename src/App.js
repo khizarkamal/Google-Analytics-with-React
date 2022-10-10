a7fa5cb4f17d811903473ea198d7491e71a2f698
Khizar Kamal <khizarkamal0314@gmail.com>
2022-10-10 15:14:03 +0500

    First Changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactGa from "react-ga";
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    ReactGa.initialize('G-FQXCWRX2GT');
+
+    ReactGa.pageview('/')
+  },[]);
+
+   
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello World
       </header>
     </div>
   );
